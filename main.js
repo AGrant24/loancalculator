d349ea637da0f632cec01832b577f956e14aeb4c
AGrant24 <alex.grant.contact@gmail.com>
2021-02-06 16:44:07 +0000

    bootstrap added, custom +/- buttons

--- main.js
+++ main.js
@@ -5,18 +5,57 @@ const loanTerm = document.querySelector("#loanTerm");
 const calcBtn = document.querySelector("#calcButton");
 const resetBtn = document.querySelector("#resetButton");
 const evalElement = document.querySelector(".evalElement");
+//Amount
+const amountMinus = document.querySelector(".minusButtonAmount");
+const amountPlus = document.querySelector(".plusButtonAmount");
+const amountInput = document.getElementById("amountInput").innerHTML;
+//Term
+const termMinus = document.querySelector(".minusButtonTerm");
+const termPlus = document.querySelector(".plusButtonTerm");
+const termInput = document.getElementById("termInput").innerHTML;
+
+// Amount button selectors
+// Plus button
+const moneyUp = function moneyUp() {
+  document.getElementById("amountInput").stepUp(1);
+};
+amountPlus.addEventListener("click", moneyUp);
+
+// Minus Button
+const moneyDown = function moneyDown() {
+  document.getElementById("amountInput").stepUp(-1);
+};
+amountMinus.addEventListener("click", moneyDown);
+
+// Term button selectors
+// Plus button
+const termUp = function termUp() {
+  document.getElementById("termInput").stepUp(1);
+};
+termPlus.addEventListener("click", termUp);
+
+// Minus Button
+const termDown = function termDown() {
+  document.getElementById("termInput").stepUp(-1);
+};
+termMinus.addEventListener("click", termDown);
 
 const initApp = function initApp() {
-  document.querySelector("#loanValue").value = "0";
-  document.querySelector("#loanTerm").value = "0";
+  // document.querySelector("#loanValue").value = "0";
+  // document.querySelector("#loanTerm").value = "0";
+  // amountInput.value = "";
+  document.getElementById("termInput").value = "";
+  document.getElementById("amountInput").value = "";
   evalElement.textContent = "";
 };
 
 initApp();
 
 const calcPayment = () => {
-  var lv = document.getElementById("loanValue").value;
-  var lt = document.getElementById("loanTerm").value;
+  // var lv = document.getElementById("loanValue").value;
+  // var lt = document.getElementById("loanTerm").value;
+  var lv = document.getElementById("amountInput").value;
+  var lt = document.getElementById("termInput").value;
 
   if (lv || lt > 0) {
     if (lv > 1000) {
