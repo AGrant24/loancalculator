95ca7328d091f320effcc2ed127c986e2f8a3ace
AGrant24 <alex.grant.contact@gmail.com>
2021-04-05 20:05:16 +0100

    replaced var with let

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ amountPlus.addEventListener("click", moneyUp);
 
 // Minus Button
 const moneyDown = function moneyDown() {
-  document.getElementById("amountInput").stepUp(-1);
+  document.getElementById("amountInput").stepDown(1);
 };
 amountMinus.addEventListener("click", moneyDown);
 
@@ -36,14 +36,11 @@ termPlus.addEventListener("click", termUp);
 
 // Minus Button
 const termDown = function termDown() {
-  document.getElementById("termInput").stepUp(-1);
+  document.getElementById("termInput").stepDown(1);
 };
 termMinus.addEventListener("click", termDown);
 
 const initApp = function initApp() {
-  // document.querySelector("#loanValue").value = "0";
-  // document.querySelector("#loanTerm").value = "0";
-  // amountInput.value = "";
   document.getElementById("termInput").value = "";
   document.getElementById("amountInput").value = "";
   evalElement.textContent = "";
@@ -52,44 +49,33 @@ const initApp = function initApp() {
 initApp();
 
 const calcPayment = () => {
-  // var lv = document.getElementById("loanValue").value;
-  // var lt = document.getElementById("loanTerm").value;
-  var lv = document.getElementById("amountInput").value;
-  var lt = document.getElementById("termInput").value;
+  let lv = document.getElementById("amountInput").value;
+  let lt = document.getElementById("termInput").value;
+  let result;
+  let interestRate;
 
   if (lv || lt > 0) {
     if (lv > 1000) {
-      var result = (lv / lt / 100) * 120;
-      var interestRate = "20%";
+      result = (lv / lt / 100) * 120;
+      interestRate = "20%";
       // 20% interest
     } else {
-      var result = (lv / lt / 100) * 140;
-      var interestRate = "40%";
+      result = (lv / lt / 100) * 140;
+      interestRate = "40%";
       // 40% interest
     }
     const youPay = parseFloat(result).toFixed(2);
     const totalPay = youPay * lt;
 
-    // did the user enter 0's? if not calculate the loan
     if (lv > 0 && lt > 0) {
       evalElement.textContent = `If you borrow £${lv} over ${lt} months, you will pay £${youPay} each month for a total of £${totalPay}. 
    Your interest rate will be ${interestRate}.`;
-      // add apply now btn from elements
     } else {
       initApp();
       alert("Values need to be more than '0'");
-      //style this
     }
   }
 };
 
 calcBtn.addEventListener("click", calcPayment);
 resetBtn.addEventListener("click", initApp);
-
-// Style the form validation
-
-// Produce an apply now button when valid entry using createElement
-
-// Apply spinner while loading
-
-// Accepted or Rejected function
